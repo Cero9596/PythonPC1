bd8b6f002141ad4e9f5e3418913995413354f223
Cero9596 <cero.cro@gmail.com>
2024-05-04 06:02:49 +0000

    Problema 5

---
+++ Problema5.py
@@ -0,0 +1,8 @@
+cantidad_shows = int(input("Ingrese la cantidad de shows musicales que ha visto en el último año: "))
+
+if cantidad_shows >= 3:
+    True
+    print("El usuario si ha visto 3 o más shows.")
+else:
+    False
+    print("El usuario no ha visto 3 o más shows.")
